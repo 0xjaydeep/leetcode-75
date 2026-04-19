4dbd60bce99884fa869f56d496621c852939b3e0
0xjaydeep <jaydeeplc@hotmail.com>
2026-04-20 01:34:35 +0530

    LC-75: 1079 GCD Of Strings

---
+++ src/main/java/com/jaydeep/lc75/arrays_hashing/Solution1079.java
@@ -0,0 +1,30 @@
+package com.jaydeep.lc75.arrays_hashing;
+
+public class Solution1079 {
+    class Solution {
+        public String gcdOfStrings(String str1, String str2) {
+            // Get the GCD of two Strings
+            int gcdVal = gcd(str1.length(), str2.length());
+            // GCD Candidate
+            String result = str1.substring(0, gcdVal);
+
+            if(isDivisible(str1, result) && isDivisible(str2, result)) return result;
+            return "";
+
+        }
+
+        private int gcd(int a, int b) {
+            return b == 0 ? a : gcd(b, a % b);
+        }
+
+        private boolean isDivisible(String str, String result) {
+            int chunks = str.length() / result.length();
+            for(int i = 0; i < chunks; i++) {
+                String chunk = str.substring(i * result.length(), (i + 1) * result.length()) ;
+                if(!chunk.equals(result)) return false;
+
+            }
+            return true;
+        }
+    }
+}
